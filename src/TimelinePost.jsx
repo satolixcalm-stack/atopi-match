78076b52d0169310e466b73dd5743b830874f636
satolixcalm-stack <satolixcalm@gmail.com>
2026-05-02 21:53:45 +0900

    Update TimelinePost.jsx

--- src/TimelinePost.jsx
+++ src/TimelinePost.jsx
@@ -291,7 +291,7 @@ comments.forEach(c => {
   }}
 >
    {/* 右上アクションエリア（投稿） */}
-<div style={{ position:"absolute", top:0, right:6, width:60, display:"flex", justifyContent:"flex-end", alignItems:"center", gap:6 }}>
+<div style={{ position:"absolute", top:12, right:6, width:60, display:"flex", justifyContent:"flex-end", alignItems:"center", gap:6 }}>
   {!isOwner && (
     <button
       onClick={() => handleReply({
